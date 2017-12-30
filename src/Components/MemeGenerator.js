862c69c562150191970e142bb95b0b3aa6b95ab9
rjgagui <robert.gaguitr@gmail.com>
2017-12-30 13:08:38 +0800

    added state on MemeGenerator

--- src/Components/MemeGenerator.js
+++ src/Components/MemeGenerator.js
@@ -1,17 +1,40 @@
 import React,{Component} from 'react';
 import {Form,FormGroup,FormControl,ControlLabel,Button} from 'react-bootstrap';
+
 class MemeGenerator extends Component{
+  constructor(){
+    super();
+    this.state={
+      'top-text':'',
+      'bottom-text':''
+    }
+  }
+
+  handleTextChange(e){
+    this.setState({[e.target.name]:e.target.value});
+  }
+
   render(){
     return(
       <Form inline>
         <FormGroup>
           <ControlLabel>Top</ControlLabel>
-          <FormControl type='Text' placeholder='top text'></FormControl>
+          <FormControl type='Text'
+            name='top-text'
+            placeholder='top text'
+            onChange={this.handleTextChange.bind(this)}
+            value={this.state['top-text']}
+          />
         </FormGroup>
-        <formGroup>
+        <FormGroup>
           <ControlLabel>Bottom</ControlLabel>
-          <FormControl type="text" placeholder="botton text"></FormControl>
-        </formGroup>
+          <FormControl type="text"
+            name="bottom-text"
+            placeholder="botton text"
+            onChange={this.handleTextChange.bind(this)}
+            value={this.state['bottom-text']}
+          />
+        </FormGroup>
       </Form>
     )
   }
