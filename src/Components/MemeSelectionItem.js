89972fa053f79984d720d48823727b17b74f4a66
rjgagui <robert.gaguitr@gmail.com>
2017-12-30 18:24:05 +0800

    added hovered state on meme item. added animation style on hover

--- src/Components/MemeSelectionItem.js
+++ src/Components/MemeSelectionItem.js
@@ -1,13 +1,32 @@
 import React,{Component} from 'react';
 
 class MemeSelectionItem extends Component{
+
+  constructor(){
+    super();
+    this.state={
+      hovered:false
+    };
+  }
+
   render(){
     const {url,name} = this.props.meme;
+    const {hovered} = this.state;
+    let imgStyle = 'meme-img';
+    let txtStyle = 'meme-text';
+
+    if(hovered){
+      imgStyle +=' darken-img';
+      txtStyle +=' no-txt';
+    }
+
     return(
-      <div className="meme-item" >
+      <div className="meme-item"
+        onMouseOver={()=>this.setState({hovered:true})}
+        onMouseLeave={()=>this.setState({hovered:false})}>
         <img src={url} alt={name}
-          className="meme-img"/>
-          <p className="meme-text">{name}</p>
+          className={imgStyle}/>
+          <p className={txtStyle}>{name}</p>
       </div>
     )
   }
